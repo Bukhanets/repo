02eab76902c665589bf2948346e905c11e93a6dd
Yurii Bukhanets <s24715@szuflandia.pjwstk.edu.pl>
2021-11-06 16:44:40 +0100

    pierwsza

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+int a, b, c, srednia;
+std::cout << "Podaj liczby całkowite";
+std::cin >> a >> b >> c;
+srednia = (a+b+c)/3;
+std::cout<< "suma = " << suma;
+return 0;
+}
+
